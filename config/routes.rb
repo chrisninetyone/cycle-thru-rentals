78e4c292c318e351f346721ff709d3bb80b5a898
Joe Walker <jpdwalker@gmail.com>
2019-05-28 11:18:43 +0800

    initial routes agreed

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  root to: 'pages#home'
 
   resources :bicycles
 
+  resources :bookings, only: [:create, :new, :show, :index] do
+    resources :reviews, only: [:create, :edit, :update]
+  end
   devise_for :users
-  
-  root to: 'pages#home'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
