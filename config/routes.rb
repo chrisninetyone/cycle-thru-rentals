d430bc5c76ed40d59675ffec84e264de3bfba52b
Joe Walker <jpdwalker@gmail.com>
2019-05-28 12:55:42 +0800

    added logic to bookings controller, added coloumn to booking model "confirm:boolean"

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
 
-  resources :bicycles
+  resources :bicycles do
+    resources :bookings, only: [:create]
+  end
 
-  resources :bookings, only: [:create, :new, :show, :index] do
+  resources :bookings, only: [:show, :index] do
     resources :reviews, only: [:create, :edit, :update]
   end
+
   devise_for :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_27_090801) do
+ActiveRecord::Schema.define(version: 2019_05_28_045219) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2019_05_27_090801) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "confirm"
     t.index ["bicycle_id"], name: "index_bookings_on_bicycle_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
