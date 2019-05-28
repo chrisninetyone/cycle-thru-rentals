b4b4894ab209ef968a17c2418d9556ed14ae4ee4
Joe Walker <jpdwalker@gmail.com>
2019-05-28 16:52:16 +0800

    added user dependencies, and logic to newly created bike

--- app/models/bicycle.rb
+++ app/models/bicycle.rb
@@ -1,6 +1,8 @@
 class Bicycle < ApplicationRecord
   mount_uploader :photo, PhotoUploader
 
+  belongs_to :user
+
   validates :brand, presence: true
   validates :category, presence: true
   validates :price_per_day, presence: true
