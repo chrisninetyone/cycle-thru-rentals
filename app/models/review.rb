cf5cceca0c3485b84ab330340cd0c1520114492a
Christopher Foley <chrisninetyone@gmail.com>
2019-05-29 15:52:24 +0800

    fixed redirect if review form incorrect

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :booking
+
+  validates :rating, presence: true, allow_blank: false, numericality: true
+  validates :content, presence: true, allow_blank: false
 end
