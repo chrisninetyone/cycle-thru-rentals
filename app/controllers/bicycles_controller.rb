081cc3e9dfcb36349617eece244e290d375827cb
Darren Tonks <dtonks33@gmail.com>
2019-05-30 16:01:04 +0900

    combined simple search and map markers on bicycle index.

--- app/controllers/bicycles_controller.rb
+++ app/controllers/bicycles_controller.rb
@@ -3,25 +3,13 @@ class BicyclesController < ApplicationController
   before_action :set_bicycle, only: [:edit, :destroy, :show, :update]
 
   def index
-
     if params[:query].present?
-      @bicycles_search = Bicycle.where("address ILIKE ?", "%#{params[:query]}%")
+      @bicycles = Bicycle.where("address ILIKE ?", "%#{params[:query]}%")
+      @bicycles = Bicycle.where.not(latitude: nil, longitude: nil)
     else
-      @bicycles_search = Bicycle.all
+      @bicycles = Bicycle.all
     end
-
-    @bicycles = Bicycle.all
-
-    @bicycles = Bicycle.where.not(latitude: nil, longitude: nil)
-    @markers = @bicycles.map do |bicycle|
-      {
-        lat: bicycle.latitude,
-        lng: bicycle.longitude,
-        infoWindow: render_to_string(partial: "infowindow", locals: { bicycle: bicycle }),
-        image_url: helpers.asset_url('CTlogo.png')
-      }
-
-    end
+    map_markers
   end
 
   def show
@@ -76,5 +64,16 @@ class BicyclesController < ApplicationController
 
   def bicycle_params
     params.require(:bicycle).permit(:description, :address, :price_per_day, :category, :brand, :photo)
+  end
+
+  def map_markers
+    @markers = @bicycles.map do |bicycle|
+      {
+        lat: bicycle.latitude,
+        lng: bicycle.longitude,
+        infoWindow: render_to_string(partial: "infowindow", locals: { bicycle: bicycle }),
+        image_url: helpers.asset_url('CTlogo.png')
+      }
+    end
   end
 end
