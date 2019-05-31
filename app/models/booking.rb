00a4eb760abb90293d9658ffee8f8306548ead8f
Christopher Foley <chrisninetyone@gmail.com>
2019-05-31 15:04:32 +0800

    Disabled bookings for past dates and dates where the begin is after the end date

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,7 +3,25 @@ class Booking < ApplicationRecord
   belongs_to :user
   has_many :reviews
 
+  validates :begin_date, presence: true
+  validates :end_date, presence: true
+
+  validate :end_date_after_begin_date
   def confirm!
     @confirm = true
+  end
+
+  private
+
+  def end_date_after_begin_date
+    return if end_date.blank? || begin_date.blank?
+
+    if end_date < begin_date
+      errors.add(:end_date, "must be after the start date")
+    end
+
+    if end_date < DateTime.now
+      errors.add(:end_date, "cannot be in the past")
+    end
   end
 end
