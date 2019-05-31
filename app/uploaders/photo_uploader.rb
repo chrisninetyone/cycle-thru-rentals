bfb293c834dc8a5ac0427f8a898d6168bdd67e16
Christopher Foley <chrisninetyone@gmail.com>
2019-05-31 16:14:11 +0800

    changed show page from bootstrap grid to flexbox grid

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -1,3 +1,8 @@
 class PhotoUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
+
+  version :bright_face do
+    cloudinary_transformation effect: "brightness:30", radius: 10,
+      width: 400, height: 300, crop: :fill
+  end
 end
