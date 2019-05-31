6ead714f08bd691c72cba266ffd7c5d5bc2eab27
Darren Tonks <dtonks33@gmail.com>
2019-05-31 13:37:59 +0900

    added algolia auto-fill search

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,5 +3,7 @@ import "bootstrap";
 // import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 
  import { initMapbox } from '../plugins/init_mapbox';
+ import { initAlgolia } from '../plugins/init_algolia';
 
 initMapbox();
+initAlgolia();
